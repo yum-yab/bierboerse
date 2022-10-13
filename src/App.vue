d7a112ca5b432a47d1c153ec591918e6857093b8
Denis <yum-yab-dev@pm.me>
2022-10-13 10:11:57 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Chart from './components/Chart.vue'
+</script>
+
+<template>
+  <header>
+  <h1>Bierbörse</h1>
+    
+  </header>
+
+  <main>
+    <Chart chartname="Testytest"/>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  place-items: left;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  main {
+    place-items: center;
+  }
+}
+</style>
---
+++ src/components/Chart.vue
@@ -0,0 +1,130 @@
+<script setup>
+
+defineProps({
+  chartname: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
+
+<script>
+
+import { Line } from 'vue-chartjs'
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  CategoryScale,
+  Filler
+} from "chart.js";
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  CategoryScale,
+  Filler
+);  
+
+export default {
+  name: "chart",
+  components: { Line },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
+    },
+    // scales: {
+    //       //hides the y axis
+    //       y: {
+    //         grid: {
+    //           color: "#383838",
+    //           borderColor: '#2D2D2D',
+    //         },
+    //       },
+    //       x: {
+    //         grid: {
+    //           display: false,
+    //           borderColor: '#383838',
+    //         },
+    //       },
+    //     },
+    datasetIdKey: {
+      type: String,
+      default: 'label'
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {
+        return {
+          width: "1200px",
+          height: "900px",
+        }
+      }
+    },
+    plugins: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      chartData: {
+        label: "LULUL",
+        labels: [ 1, 2, 3, 4, 5 ],
+        datasets: [ { data: [1.5, 1.4, 1.3, 1.6, 1.7] } ],
+        borderColor: "#cc222d",
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+
+</script>
+
+
+<template>
+<div class="bigchart">
+
+    <Line
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+    class="bigchart"
+    />
+
+  </div>
+
+  
+</template>
+
+<style scoped>
+
+.bigchart {
+  height: "100%";
+  width: "100%"
+}
+
+
+</style>
